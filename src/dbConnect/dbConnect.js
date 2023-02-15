1f37b99e61d3a249f185228e00cdc41a93808bfa
Kevin Darke <99612676+KangarooKafka@users.noreply.github.com>
2023-02-15 16:40:27 -0500

    Second iteration

--- src/dbConnect/dbConnect.js
+++ src/dbConnect/dbConnect.js
@@ -2,14 +2,15 @@ import mongoose from "mongoose";
 import config from "config";
 import * as process from "process";
 
-import pinoLogger from '../../logger/logger';
+import pinoLogger from '../../logger/logger.js';
+import pino from "pino";
 
 /**
  * Connects the API to MongoDB database
  */
 const dbConnect = async () => {
     // Set config and options
-    const mongoConnect = config.get('mongo');
+    const mongoConnect = 'mongodb://localhost:27017/wedding-dev';
     mongoose.set('strictQuery', false);
     const options = {
         autoIndex: true,
@@ -17,20 +18,21 @@ const dbConnect = async () => {
     };
 
     // Create logger
-    const logger = pinoLogger();
+    //const logger = pinoLogger();
+    const logger = pino;
 
     // Try to connect to the database
     try {
-        await mongoose.connect(mongoConnect.uri, options);
+        await mongoose.connect(mongoConnect, options);
 
         // Ternary to only show the URI if not in PROD since at the moment the URI includes user/pass.
         process.env.NODE_ENV !== 'prod'
-            ? logger.info(`Connected to MongoDB at url ${mongoConnect.uri}`)
-            : logger.info('Connected to MongoDB');
+            ? console.log(`Connected to MongoDB at url ${mongoConnect}`)
+            : console.log('Connected to MongoDB');
     } catch (e) {
-        logger.fatal(`There was an error loading MongoDB: ${e.message}`);
+        console.log(`There was an error loading MongoDB: ${e.message}`);
         process.exit(1);
     }
 }
 
-export default dbConnect();
+export default dbConnect;
